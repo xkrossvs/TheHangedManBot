1ca4d803fcdec9c0f156a48dee7e5a2a530d8f0a
kross <ivan.k.blintsov@gmail.com>
2024-10-29 20:45:35 +0800

    Доделали работу достижения: полный провал

--- achievement_units.py
+++ achievement_units.py
@@ -6,7 +6,14 @@ class AchievementUnits:
 
     @staticmethod
     async def complete_disaster_check(data: StatesData, bot: Bot):
+        user_id = data['chat_id']
+        achievements = users.find_one(filter={'user_id': user_id})['achievements']
+        if achievements['🥀 Полный Провал'][0]:
+            return
         if ''.join(data['text_word']).count('_') == len(data['word']):
+            achievements['🥀 Полный Провал'][0] = 1
             await bot.send_message(chat_id=data['chat_id'],
                                    text='Достижение получено: Полный провал.\n'
                                         'Проиграйте игру не угадав ни одной буквы.')
+            users.update_one(filter={'user_id': user_id},
+                             update={'$set': {'achievements': achievements}})
