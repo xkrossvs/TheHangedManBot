f6b5d47c0936e4a12d163ff045c1def622770dd5
kross <ivan.k.blintsov@gmail.com>
2025-01-30 20:41:07 +0800

    Создали приватный бот для тестов

--- config.py
+++ config.py
@@ -8,10 +8,18 @@ from pymongo.collection import Collection
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
+TEST = 0
+
 MONGO_URL = os.getenv('MONGO_URL')
 cluster = pymongo.MongoClient(MONGO_URL)
-users: Collection = cluster.the_hanged_man.users
+
+if TEST:
+    TOKEN = os.getenv('TEST_TOKEN')
+    users: Collection = cluster.the_hanged_man.users_test
+else:
+    TOKEN = os.getenv('TOKEN')
+    users: Collection = cluster.the_hanged_man.users
+
 hangs: Collection = cluster.the_hanged_man.hangs
 LOG_GROUP_ID = -1002324422338
 ADMINS = [618805465, 241322552]
